05667392383ebdeb9cb4b29711d8d7f325b3eb06
Kritzel Kratzel <kritzel.kratzel@gmx.de>
2024-10-11 16:16:04 +0200

    Added ping support for IPv6.

--- src/luaping.c
+++ src/luaping.c
@@ -27,8 +27,12 @@ Code heavily insprired by lsleep library (https://github.com/andrewstarks/lsleep
 #ifdef _WINDLL
 #include <winsock2.h>	// https://stackoverflow.com/questions/1372480
 #include <windows.h>
-#include <iphlpapi.h>
+#include <ipexport.h>
 #include <icmpapi.h>
+#include <ip2string.h>
+#include <in6addr.h>
+#define STATUS_SUCCESS 0x0	// FIXME: didn't find the corresponding header
+#include <ws2ipdef.h>
 #define DLL __declspec(dllexport)
 #else
 // #include <unistd.h>
@@ -69,141 +73,219 @@ static int luaping_timeout(lua_State *L) {
   return 1;
 }
 
+static const char* errorMnemonic(ULONG status){
+  // Error messages corresponding to
+  // https://learn.microsoft.com/en-us/windows/win32/api/ipexport/ns-ipexport-icmp_echo_reply
+  const char *errmsg;
+  switch(status) {
+  case IP_SUCCESS :
+    errmsg = "";
+    break;
+  case IP_BUF_TOO_SMALL :
+    errmsg = "The reply buffer was too small.";
+    break;
+  case IP_DEST_NET_UNREACHABLE :
+    errmsg = "The destination network was unreachable.";
+    break;
+  case IP_DEST_HOST_UNREACHABLE :
+    errmsg = "The destination host was unreachable.";
+    break;
+  case IP_DEST_PROT_UNREACHABLE :
+    errmsg = "The destination protocol was unreachable.";
+    break;
+  case IP_DEST_PORT_UNREACHABLE :
+    errmsg = "The destination port was unreachable.";
+    break;
+  case IP_NO_RESOURCES :
+    errmsg = "Insufficient IP resources were available.";
+    break;
+  case IP_BAD_OPTION :
+    errmsg = "A bad IP option was specified.";
+    break;
+  case IP_HW_ERROR :
+    errmsg = "A hardware error occurred.";
+    break;
+  case IP_PACKET_TOO_BIG :
+    errmsg = "The packet was too big.";
+    break;
+  case IP_REQ_TIMED_OUT :
+    errmsg = "The request timed out.";
+    break;
+  case IP_BAD_REQ :
+    errmsg = "A bad request.";
+    break;
+  case IP_BAD_ROUTE :
+    errmsg = "A bad route.";
+    break;
+  case IP_TTL_EXPIRED_TRANSIT :
+    errmsg = "The time to live (TTL) expired in transit.";
+    break;
+  case IP_TTL_EXPIRED_REASSEM :
+    errmsg = "The time to live expired during fragment reassembly.";
+    break;
+  case IP_PARAM_PROBLEM :
+    errmsg = "A parameter problem.";
+    break;
+  case IP_SOURCE_QUENCH :
+    errmsg = "Datagrams are arriving too fast to be processed and datagrams may have been discarded.";
+    break;
+  case IP_OPTION_TOO_BIG :
+    errmsg = "An IP option was too big.";
+    break;
+  case IP_BAD_DESTINATION :
+    errmsg = "A bad destination.";
+    break;
+  case IP_GENERAL_FAILURE :
+    errmsg = "A general failure. This error can be returned for some malformed ICMP packets.";
+    break;
+  default :
+    errmsg = "An unspecified error occurred.";
+  }
+  return errmsg;
+}
+
 static int luaping_ping(lua_State *L) {
   // This implementation is based on the code provided at:
   // https://learn.microsoft.com/de-de/windows/win32/api/icmpapi/nf-icmpapi-icmpsendecho
-  const char *ip = luaL_checkstring(L,1);
+  DWORD myTimeout = 0;
+  const char *ip = NULL;
 
+  // Check and act according to number of arguments
+  if (lua_gettop(L) == 1) {
+    // signature 1 - only one arg (the ip-address)
+    ip = luaL_checkstring(L,1);
+    // use global timeout value
+    myTimeout = timeout;
+  }
+  else if (lua_gettop(L) == 2) {
+    // signature 2 - ip-address and inidividual timeout
+    ip = luaL_checkstring(L,1);
+    // use individual timeout value
+    myTimeout= (DWORD)luaL_checkinteger(L,2);
+  }
+  else {
+    // bailing out
+    return luaL_error(L,"Wrong number of arguments.");
+  }
+
+  // FIXME name resolver
+  // https://learn.microsoft.com/en-us/windows/win32/api/ws2tcpip/nf-ws2tcpip-getaddrinfo
+  // FIXME
+  
   unsigned long ipaddr = inet_addr(ip);
-  if (ipaddr == INADDR_NONE) {
-    lua_pushboolean (L, FALSE);
-    lua_pushstring(L, "Argument is not an IP address.");
-    return 2;
-  }
+  PCSTR term;
+  IN6_ADDR ip6addr, ip6srcaddr;
+  NTSTATUS ip6stat = RtlIpv6StringToAddressA(ip, &term, &ip6addr);
 
-  HANDLE hIcmpFile = IcmpCreateFile();
-  if (hIcmpFile == INVALID_HANDLE_VALUE) {
-    lua_pushboolean (L, FALSE);
-    lua_pushstring(L, "Unable to open ICMP handle.");
-    return 2;
-  }
+  if (ipaddr != INADDR_NONE && ip6stat != STATUS_SUCCESS) {
+    // got valid IPv4 address
 
-  char SendData[32] = "Data Buffer";
-  DWORD ReplySize = sizeof(ICMP_ECHO_REPLY) + sizeof(SendData);
-  LPVOID ReplyBuffer = (VOID*) malloc(ReplySize);
-  if (ReplyBuffer == NULL) {
-    lua_pushboolean (L, FALSE);
-    lua_pushstring(L, "Unable to allocate ICMP memory.");
-    return 2;
-  }   
+    HANDLE hIcmpFile = IcmpCreateFile();
+    if (hIcmpFile == INVALID_HANDLE_VALUE) {
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Unable to open ICMP handle.");
+      return 2;
+    }
 
-  DWORD dwRetVal = IcmpSendEcho(hIcmpFile, ipaddr, SendData, sizeof(SendData), 
-				NULL, ReplyBuffer, ReplySize, timeout);
+    char SendData[32] = "Data Buffer";
+    DWORD ReplySize = sizeof(ICMP_ECHO_REPLY) + sizeof(SendData);
+    LPVOID ReplyBuffer = (VOID*) malloc(ReplySize);
+    if (ReplyBuffer == NULL) {
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Unable to allocate ICMP memory.");
+      return 2;
+    }   
 
-  if (dwRetVal != 0) {
-    PICMP_ECHO_REPLY pEchoReply = (PICMP_ECHO_REPLY) ReplyBuffer;
-    const char *errmsg;
-    int ping_result;
+    DWORD dwRetVal = IcmpSendEcho(hIcmpFile, ipaddr, SendData, sizeof(SendData), 
+				  NULL, ReplyBuffer, ReplySize, myTimeout);
 
-    switch(pEchoReply->Status) {
-    case IP_SUCCESS :
-      errmsg = "";
-      ping_result = TRUE;
-      break;
-    case IP_BUF_TOO_SMALL :
-      errmsg = "The reply buffer was too small.";
-      ping_result = FALSE;
-      break;
-    case IP_DEST_NET_UNREACHABLE :
-      errmsg = "The destination network was unreachable.";
-      ping_result = FALSE;
-      break;
-    case IP_DEST_HOST_UNREACHABLE :
-      errmsg = "The destination host was unreachable.";
-      ping_result = FALSE;
-      break;
-    case IP_DEST_PROT_UNREACHABLE :
-      errmsg = "The destination protocol was unreachable.";
-      ping_result = FALSE;
-      break;
-    case IP_DEST_PORT_UNREACHABLE :
-      errmsg = "The destination port was unreachable.";
-      ping_result = FALSE;
-      break;
-    case IP_NO_RESOURCES :
-      errmsg = "Insufficient IP resources were available.";
-      ping_result = FALSE;
-      break;
-    case IP_BAD_OPTION :
-      errmsg = "A bad IP option was specified.";
-      ping_result = FALSE;
-      break;
-    case IP_HW_ERROR :
-      errmsg = "A hardware error occurred.";
-      ping_result = FALSE;
-      break;
-    case IP_PACKET_TOO_BIG :
-      errmsg = "The packet was too big.";
-      ping_result = FALSE;
-      break;
-    case IP_REQ_TIMED_OUT :
-      errmsg = "The request timed out.";
-      ping_result = FALSE;
-      break;
-    case IP_BAD_REQ :
-      errmsg = "A bad request.";
-      ping_result = FALSE;
-      break;
-    case IP_BAD_ROUTE :
-      errmsg = "A bad route.";
-      ping_result = FALSE;
-      break;
-    case IP_TTL_EXPIRED_TRANSIT :
-      errmsg = "The time to live (TTL) expired in transit.";
-      ping_result = FALSE;
-      break;
-    case IP_TTL_EXPIRED_REASSEM :
-      errmsg = "The time to live expired during fragment reassembly.";
-      ping_result = FALSE;
-      break;
-    case IP_PARAM_PROBLEM :
-      errmsg = "A parameter problem.";
-      ping_result = FALSE;
-      break;
-    case IP_SOURCE_QUENCH :
-      errmsg = "Datagrams are arriving too fast to be processed and datagrams may have been discarded.";
-      ping_result = FALSE;
-      break;
-    case IP_OPTION_TOO_BIG :
-      errmsg = "An IP option was too big.";
-      ping_result = FALSE;
-      break;
-    case IP_BAD_DESTINATION :
-      errmsg = "A bad destination.";
-      ping_result = FALSE;
-      break;
-    case IP_GENERAL_FAILURE :
-      errmsg = "A general failure. This error can be returned for some malformed ICMP packets.";
-      ping_result = FALSE;
-      break;
-    default :
-      errmsg = "An unspecified error occurred.";
-      ping_result = FALSE;
-    }    
-    lua_pushboolean (L, ping_result);
-    if (errmsg[0] == '\0') {
-      lua_pushnil(L);	// empty errmsg
+    if (dwRetVal != 0) {
+      PICMP_ECHO_REPLY pEchoReply = (PICMP_ECHO_REPLY) ReplyBuffer;
+      const char *errmsg = errorMnemonic(pEchoReply->Status);
+      if (strcmp(errmsg,"") == 0) {
+	// no error, ping successful
+	lua_pushboolean (L, TRUE);
+	lua_pushnil(L);	// empty errmsg
+      }
+      else {
+	// some error occured
+	lua_pushboolean (L, FALSE);
+	lua_pushstring(L, errmsg);
+      }
     }
     else {
-      lua_pushstring(L, errmsg);
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Call to IcmpSendEcho failed.");
     }
+    free(ReplyBuffer);
+    IcmpCloseHandle(hIcmpFile);
+    return 2;
+  }
+  else if (ipaddr == INADDR_NONE && ip6stat == STATUS_SUCCESS) {
+    // got valid IPv6 address
+
+    HANDLE hIcmp6File = Icmp6CreateFile();
+    if (hIcmp6File == INVALID_HANDLE_VALUE) {
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Unable to open ICMP6 handle.");
+      return 2;
+    }
+
+    char SendData[32] = "Data Buffer";
+    DWORD ReplySize = sizeof(ICMPV6_ECHO_REPLY) + sizeof(SendData) + 8;
+    LPVOID ReplyBuffer = (VOID*) malloc(ReplySize);
+    if (ReplyBuffer == NULL) {
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Unable to allocate ICMPV6 memory.");
+      return 2;
+    }   
+
+    // There is no Icmp6SendEcho() function in analogy to IcmpSendEcho() for IPv4.
+    // Closest neighbour is Icmp6SendEcho2() which requires an explicit source address;
+    // we simply use IPv6 localhost as source address.
+    RtlIpv6StringToAddressA("::1", &term, &ip6srcaddr);
+    SOCKADDR_IN6 srcAddr;
+    memset(&srcAddr, 0, sizeof(srcAddr));
+    srcAddr.sin6_family = AF_INET6;
+    srcAddr.sin6_addr = ip6srcaddr;
+
+    // Fill IPv6 target address
+    SOCKADDR_IN6 destAddr;
+    memset(&destAddr, 0, sizeof(destAddr));
+    destAddr.sin6_family = AF_INET6;
+    destAddr.sin6_addr = ip6addr;
+
+    DWORD dwRetVal = Icmp6SendEcho2(hIcmp6File, NULL, NULL, NULL, &srcAddr, &destAddr,
+				    SendData, sizeof(SendData), 
+				    NULL, ReplyBuffer, ReplySize, myTimeout);
+
+    if (dwRetVal != 0) {
+      PICMPV6_ECHO_REPLY pV6EchoReply = (PICMPV6_ECHO_REPLY) ReplyBuffer;
+      const char *errmsg = errorMnemonic(pV6EchoReply->Status);
+      if (strcmp(errmsg,"") == 0) {
+	// no error, ping successful
+	lua_pushboolean (L, TRUE);
+	lua_pushnil(L);	// empty errmsg
+      }
+      else {
+	// some error occured
+	lua_pushboolean (L, FALSE);
+	lua_pushstring(L, errmsg);
+      }
+    }
+    else {
+      lua_pushboolean (L, FALSE);
+      lua_pushstring(L, "Call to Icmp6SendEcho2 failed.");
+    }
+    free(ReplyBuffer);
+    IcmpCloseHandle(hIcmp6File);
+    return 2;    
   }
   else {
     lua_pushboolean (L, FALSE);
-    lua_pushstring(L, "Call to IcmpSendEcho failed.");
+    lua_pushstring(L, "Argument is not an IPv4 or IPv6 address.");
+    return 2;
   }
-  free(ReplyBuffer);
-  return 2;
 }
 #else
 // FIXME - non-_WINDLL not yet implemented
